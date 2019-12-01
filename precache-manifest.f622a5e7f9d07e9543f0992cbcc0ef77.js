ff91d0bec87f9298846ce1524f9f3f681cd40a77
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-12-01 20:26:24 +0300

    Updates

---
+++ precache-manifest.f622a5e7f9d07e9543f0992cbcc0ef77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1fe2bdbea3ad9224fde0521ff34590",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "82cbfafa0053d8100dc2",
+    "url": "/social-network/static/css/3.f9239a27.chunk.css"
+  },
+  {
+    "revision": "b840c2be7e16b255ae05",
+    "url": "/social-network/static/css/main.5a08873a.chunk.css"
+  },
+  {
+    "revision": "652773b96c093db0be0d",
+    "url": "/social-network/static/js/2.45b40759.chunk.js"
+  },
+  {
+    "revision": "82cbfafa0053d8100dc2",
+    "url": "/social-network/static/js/3.1adf028e.chunk.js"
+  },
+  {
+    "revision": "b840c2be7e16b255ae05",
+    "url": "/social-network/static/js/main.f7ee6445.chunk.js"
+  },
+  {
+    "revision": "b15211ca00909c933dae",
+    "url": "/social-network/static/js/runtime-main.c6aa3570.js"
+  },
+  {
+    "revision": "89865d3578fd21d63b0c4a1ca83d0a34",
+    "url": "/social-network/static/media/headerImage.89865d35.jpg"
+  },
+  {
+    "revision": "0eb301b58038868a5cc4056c74c3e9e2",
+    "url": "/social-network/static/media/image.0eb301b5.JPG"
+  },
+  {
+    "revision": "d9933c4e2c272f33b74ef18cdf11a7d5",
+    "url": "/social-network/static/media/loader.d9933c4e.gif"
+  }
+]);
